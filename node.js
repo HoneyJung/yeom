04067fe9a655b551df5470d4bff825ec81614800
Honey <kinkin21c@u.sogang.ac.kr>
2020-12-02 01:25:41 +0900

    chunk

--- node.js
+++ node.js
@@ -15,6 +15,9 @@ function main() {
     }
 }
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}	
 function check(text){
 	let newtext = ''
 	let flag = 0;
@@ -86,63 +89,129 @@ function sort_dict(dict){
 	});
 	let new_items = [];
 	len = items.length;
-	let 5per = parseInt(items.length * 0.05);
-	let 25per = parseInt(items.length * 0.025);
+	let fiveper = parseInt(items.length * 0.05);
+	let twofiveper = parseInt(items.length * 0.025);
 	let center = parseInt(items.length/2);
-	for(let i=0;i<5per;i++)
+	for(let i=0;i<fiveper;i++)
 	{
 		new_items.push([i,items[i][0],items[i][1],"popular"]); 
 	}
-	for(let i=center-25per; i < center+25per; i++){
+	for(let i=center-twofiveper; i < center+twofiveper; i++){
 		new_items.push([i,items[i][0],items[i][1],"common"]); 
 	}
-	for(let i=len-5per; i<len; i++){
+	for(let i=len-fiveper; i<len; i++){
 		new_items.push([i,items[i][0],items[i][1],"rare"]); 
 	}
 	return new_items;
 }
+/*
+function post(request,option,new_dict){
+	request.post(option,function(error,res,body){
+		let res_dict = res.body;
+		console.log(error,JSON.parse(res.body).only);
+		for(let key in res_dict){
+			if(key in new_dict){
+				new_dict[key]++;
+			}
+			else{
+				new_dict[key] = 1;
+			}
+		}
+		console.log(Object.keys(new_dict).length);
+	})
+}
 
-function master() {
+function post3(new_dict,op1,op2,op3,request){
+	post(request, op1,new_dict);
+	post(request, op2,new_dict);
+	post(request, op3,new_dict);
+}*/
+
+async function master() {
     const inputFileName = process.argv[2];
+	//const chunksize = process.argv[3];
     const fs = require('fs');
 	let text = fs.readFileSync("./" + inputFileName, {encoding: 'utf8', flag: 'r'});
    	console.log("MASTER3"); 
 	const request = require('request');
 
 	let text_split = text.split(' ');
-	length = text_split.length;
-	split_one = text_split.slice(0,length/3);
-	split_two = text_split.slice(length/3+1, (length/3) * 2);
-	split_three = text_split.slice((length/3)*2+1,length) ;
-	
+	//split_one = text_split.slice(0,length/3);
+	//split_two = text_split.slice(length/3+1, (length/3) * 2);
+	//split_three = text_split.slice((length/3)*2+1,length) ;
+	//for(let i=0; i<split_two.length; i++){
+	let chunk;
+	if(process.env.CHUNK===undefined){
+		chunk = 3;
+	}
+	else{
+		chunk = process.env.CHUNK;
+	}
+	console.log(chunk);
 	var bodyParser = require('body-parser');
 	app.use(express.urlencoded({limit: '20mb', extended: true}));
 	app.use(express.json({limit: '20mb'}));
+	
+	let new_dict = {};
+	length = text_split.length;
+	c = length / chunk;
+	for(let k=0;k<chunk;k++){
+		split = text_split.slice(c*k, c*(k+1));
+		var post_options = {
+  	    	uri: 'http://34.64.236.15:3000/reduce',
+			body: JSON.stringify({'text':split}),
+      		headers: {
+ 				'Content-Type': 'application/json'
+      		}
+  		};
+		request.post(post_options,function(error,res,body){
+			console.log("------------------",k);
+			res_dict = res.body;
+			console.log(error,JSON.parse(res.body).only);
+			for(let key in res_dict){
+				if(key in new_dict){
+					new_dict[key]++;
+				}
+				else{
+					new_dict[key] = 1;
+				}
+			}
+			console.log(Object.keys(new_dict).length);
+			console.log(new_dict.only);
+		})
+		await sleep(10000);	
+	
+	}
 
+/*
   	var post_options = {
-  	    uri: 'http://10.15.250.46:3000/reduce',
+  	    uri: 'http://34.64.236.15:3000/reduce',
 		body: JSON.stringify({'text':split_one}),
       	headers: {
  			'Content-Type': 'application/json'
       	}
   	};
+	
 	var post_options2 = {
-  	    uri: 'http://10.15.253.192:3000/reduce',
+  	    uri: 'http://34.64.236.15:3000/reduce',
 		body: JSON.stringify({'text':split_two}),
       	headers: {
  			'Content-Type': 'application/json'
       	}
   	};
   	var post_options3 = {
-  	    uri: 'http://10.15.245.200:3000/reduce',
+  	    uri: 'http://34.64.236.15:3000/reduce',
 		body: JSON.stringify({'text':split_three}),
       	headers: {
  			'Content-Type': 'application/json'
       	}
   	};
-	let new_dict = {};
+	
+	//await post3(new_dict,post_options,post_options2,post_options3, request);
+	//console.log(Object.keys(new_dict).length);
 
 	request.post(post_options,function(error,res,body){
+		console.log("-----------------1");
 		res_dict = res.body;
 		console.log(error,JSON.parse(res.body).only);
 		for(let key in res_dict){
@@ -156,7 +225,9 @@ function master() {
 		console.log(Object.keys(new_dict).length);
 		console.log(new_dict.only);
 	})
+	sleep(10000);
 	request.post(post_options2,function(error,res,body){
+		console.log("-----------------2");
 		res_dict2 = res.body;
 		console.log(error,JSON.parse(res.body).only);
 		for(let key in res_dict2){
@@ -170,7 +241,10 @@ function master() {
 		console.log(Object.keys(new_dict).length);
 		console.log(new_dict.only);
 	})
+	
+	sleep(10000);
 	request.post(post_options3,function(error,res,body){
+		console.log("-----------------3");
 		res_dict3 = res.body;
 		console.log(error,JSON.parse(res.body).only);
 		for(let key in res_dict3){
@@ -184,8 +258,8 @@ function master() {
 		console.log(Object.keys(new_dict).length);
 		console.log(new_dict.only);
 	})
-	result = sort_dict(new_dict);
-	console.log(result);
-
 	
+	//result = sort_dict(new_dict);
+	//console.log(result);
+*/	
 }
